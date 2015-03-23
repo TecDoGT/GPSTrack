f5ebc4956b322db3182212369feaefb6c4145fe7
TecDoGT <ddelaguila@tecdogt.com>
2015-03-23 15:52:35 -0600

    Cambios menores, faltas ortgraficas

--- www/js/home.js
+++ www/js/home.js
@@ -10,10 +10,26 @@ else
 function getGeoPoints()
 {
 	if (window.sessionStorage.UserLogin && window.sessionStorage.UserCode && window.sessionStorage.UserEmpresa)
-	{}
+	{
+		//get datos...
+	}
 	else
 	{
-		
+		new Messi('Error, Faltan par&aacute;metros... por favor reinicie la aplicaci&oacute;n', 
+				{
+					title: 'TecDo GPS Tracker', 
+					titleClass: 'anim error', 
+					buttons: 
+						[
+							{
+								id: 0, 
+								label: 'Cerrar', 
+								val: 'X'
+							}
+						],
+					modal: true,
+					width: (window.innerWidth - 25)
+				});
 	}
 	
 }
